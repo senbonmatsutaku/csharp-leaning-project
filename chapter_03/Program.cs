ebefb8169049cb3d5ad22005633d46550815565e
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-04-20 23:04:14 +0900

    Add: ３章課題を追加

---
+++ chapter_03/Program.cs
@@ -0,0 +1,13 @@
+﻿using chapter_03.controller;
+using System;
+
+namespace chapter_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainController.Run();
+        }
+    }
+}
