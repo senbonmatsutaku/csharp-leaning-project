482e13aa5eff0929fc36721a93adad0015b3a57f
NRS <NRS@DESKTOP-6B0PUE5>
2019-04-20 15:26:32 +0900

    Fix:指摘点の修正

--- chapter_01/domain/service/TaskServiceImplementedBy667.cs
+++ chapter_01/domain/service/TaskServiceImplementedBy667.cs
@@ -30,7 +30,7 @@ namespace chapter_01.domain.service
             Console.WriteLine(flg);
             Console.WriteLine(syosuu);
             Console.WriteLine(largeValue);
-            Console.WriteLine(mozi2);
+            Console.WriteLine(moji2);
         }
 
         // 問1
