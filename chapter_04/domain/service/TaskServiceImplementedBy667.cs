42a3050e98020f136bc02906186107d55f473c93
NRS <NRS@DESKTOP-6B0PUE5>
2019-05-25 22:49:52 +0900

    fix:課題4を実施

--- chapter_04/domain/service/TaskServiceImplementedBy667.cs
+++ chapter_04/domain/service/TaskServiceImplementedBy667.cs
@@ -8,14 +8,40 @@ namespace chapter_04.domain.service
     {
         public void CountGame()
         {
+            int[] numbers = { 3, 4, 9 };
+            Console.Write("１桁の数字を入力してください >");
+            var input = int.Parse(Console.ReadLine());
+            for(int i=0; i<2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (numbers[j] == input)
+                    {
+                        Console.WriteLine("アタリ");
+                        return;
+                    }
+                }
+                Console.WriteLine("ハズレ");
+                Console.Write("１桁の数字を入力してください >");
+                input = int.Parse(Console.ReadLine());
+            }
         }
 
         public void LearnArrayDeclaration()
         {
+            int[] points = new int[4];
+            double[] weights = new double[4];
+            bool[] answers = new bool[4];
+            String[] names = new String[4];
         }
 
         public void LearnArrayFactorDisplay()
         {
+            int[] moneyList = { 121902, 8302, 55100 };
+            for(int i = 0; i<3; i++)
+            {
+                Console.WriteLine(moneyList[i]);
+            }
         }
 
         // 下記のコードを実行すると、29行目と30行目で例外が発生する
@@ -27,7 +53,9 @@ namespace chapter_04.domain.service
             int[] count = null;
             float[] heights = { 171.3F, 175.0F };
             //Console.WriteLine(count[1]);
+            Console.WriteLine("ここではSystem.NullReferenceExceptionという例外が発生"); 
             //Console.WriteLine(heights[2]);
+            Console.WriteLine("ここではSystem.IndexOutOfRangeExceptionという例外が発生");
         }
     }
 }
