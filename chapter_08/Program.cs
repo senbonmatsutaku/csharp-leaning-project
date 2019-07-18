76ba227643522ba45e31040462f9ade2b8d3b567
Shinichi Kaneko <kuturogi99with99jazz@yahoo.co.jp>
2019-07-19 05:41:45 +0900

    Add: ８章課題を作成

---
+++ chapter_08/Program.cs
@@ -0,0 +1,13 @@
+﻿using chapter_08.controller;
+using System;
+
+namespace chapter_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainController.Run();
+        }
+    }
+}
