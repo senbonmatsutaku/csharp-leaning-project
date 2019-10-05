c73006ea91217eacf0dcc49f9548af3c2d229efb
NRS <NRS@DESKTOP-6B0PUE5>
2019-10-05 13:46:26 +0900

    add:課題11を実施

--- chapter_11/domain/service/student667/Task667.cs
+++ chapter_11/domain/service/student667/Task667.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace chapter_11.domain.service.student667
 {
@@ -8,22 +9,46 @@ namespace chapter_11.domain.service.student667
     {
         public void Task1()
         {
-            return;
+            string s;
+            s = null;
+            //Console.WriteLine(s.Length);
         }
 
         public void Task2()
         {
-            return;
+            try
+            {
+                string s;
+                s = null;
+                Console.WriteLine(s.Length);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("NullReferenceException例外をcatchしました。");
+                Console.WriteLine("－－スタックトレース（ここから）－－");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("－－スタックトレース（ここまで）－－");
+            }
         }
 
         public void Task3()
         {
-            return;
+            try
+            {
+                int result = Int32.Parse("三");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("FormatException例外をcatchしました。");
+                Console.WriteLine("－－スタックトレース（ここから）－－");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("－－スタックトレース（ここまで）－－");
+            }
         }
 
         public void Task4()
         {
-            return;
+                StreamReader Reader = File.OpenText("A.txt");
         }
     }
 }
